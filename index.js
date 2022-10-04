4f73031826141439ff70092320c05ce2a18f1628
ThomassMathisen <thomasmathisen@hotmail.com>
2022-10-04 21:35:34 +0200

    Fix code

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import dotenv from "dotenv"
 import http from "http"
 import fs from "fs/promises"
+import path from 'path';
 dotenv.config()
 
 const port = process.env.PORT || 3000
 
-const app = http.createServer( async (res, req) => {
+const app = http.createServer( async (req, res) => {
   res.statusCode = 200
-  res.setHeader("Content-type", "text/html")
-  if (req.url === "/about") {
-    res.end(await fs.readFile("./pages/about.html"))
-  } else if (req.url === "/") {
+  res.setHeader("Content-Type", "text/html")
+  if (req.url === "/") {
     res.end(await fs.readFile("./pages/index.html"))
+  } else if (req.url === "/about") {
+    res.end(await fs.readFile("./pages/about.html"))
   } else if (req.url === "/contact-me") {
     res.end(await fs.readFile("./pages/contact-me.html"))
   } else {
